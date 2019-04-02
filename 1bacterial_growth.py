c301b513687ecb09d30c4e682ac58da726c077e7
11027649 <natasjawezel@hotmail.com>
2019-04-02 14:50:17 +0200

    fitting function

--- 1bacterial_growth.py
+++ 1bacterial_growth.py
@@ -1,12 +1,28 @@
 import matplotlib.pyplot as plt
-import seaborn as sns
-sns.set()
+import scipy as sp
+import numpy as np
+
+from scipy.optimize import curve_fit
+# import seaborn as sns
+# sns.set()
 
 def main():
-    bacterial_density = [10, 20]
-    hours = [0, 20]
+    # bacterial density doubles in 20 hours
+    x = [10, 20]
+    t = [0, 20]
 
-    growth = C * x
+    # where D is the integration constant and C the original constant
+    # ydata = curve_fit(lambda t, D, C: D * np.exp(C * t), x, t, p0=(10, 0.03), maxfev=100000)
+    #
+    time = np.arange(0, 25, 1)
+    ydata = 10 * np.exp(0.03 * time)
+    #
+    print(ydata)
+    #
+    plt.plot(time, ydata)
+    plt.scatter(t,x)
+    plt.ylim(0, 100)
+    plt.show()
 
 if __name__ == '__main__':
     main()
